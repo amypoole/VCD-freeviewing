ebc163d6885069f25d2917c690c70fb52ae077ec
Amy Poole <poole163@umn.edu>
2025-10-21 12:22:12 -0500

    runme changes from PP room testing

--- scripts/runmevcdfv.m
+++ scripts/runmevcdfv.m
@@ -57,11 +57,10 @@ function [] = runmevcdfv(subjn, runn, wantnewruns, env, wanteyetracking, skipsyn
 %                   trialoffsets:
 %
 %                   
-% ex: runmevcdfv(1, 1, true, 'AMY', false, 1)
+% ex: runmevcdfv(99, 1, true, 'PP', true, 1)
+%     runmevcdfv(99, 2, false, 'PP', true, 1)
 
 %% deal with inputs/pathing/variables
-
-rng('shuffle');
 
 % paths -------------------------------------------------------------------
 switch env
@@ -69,17 +68,10 @@ switch env
         ptb_dir     = '/Users/lana/Documents/MATLAB/Psychtoolbox-3-3.0.17.3'; addpath(genpath(ptb_dir));
         knk_dir     = '/Users/lana/Documents/MATLAB/knkutils'; addpath(genpath(knk_dir));
         git_dir     = '/Users/lana/Desktop/VCD-freeviewing'; addpath(genpath(git_dir));
-        stimuli_dir = '/Users/lana/Desktop/VCD-freeviewing/stimuli';
-        run_filedir = sprintf('Users/lana/Desktop/VCD-freeviewing/data/sub%.02d/run%.02d', subjn, runn);
-        subj_filedir = sprintf('/Users/lana/Desktop/VCD-freeviewing/data/sub%.02d/info', subjn);
     case 'PP'
-        % dirs FIX FOR PPROOM!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-        ptb_dir     = '/Users/lana/Documents/MATLAB/Psychtoolbox-3-3.0.17.3'; addpath(genpath(ptb_dir));
-        knk_dir     = '/Users/lana/Documents/MATLAB/knkutils'; addpath(genpath(knk_dir));
-        git_dir     = '/Users/lana/Desktop/VCD-freeviewing'; addpath(genpath(git_dir));
-        stimuli_dir = '/Users/lana/Desktop/VCD-freeviewing/stimuli';
-        run_filedir = sprintf('Users/lana/Desktop/VCD-freeviewing/data/sub%.02d/run%.02d', subjn, runn);
-        subj_filedir = sprintf('/Users/lana/Desktop/VCD-freeviewing/data/sub%.02d/info', subjn);
+        ptb_dir     = '/Applications/Psychtoolbox'; addpath(genpath(ptb_dir));
+        knk_dir     = '/Users/psphuser/Desktop/cvnlab/VCD-freeviewing/knkutils'; addpath(genpath(knk_dir));
+        git_dir     = '/Users/psphuser/Desktop/cvnlab/VCD-freeviewing'; addpath(genpath(git_dir));
         %screen
         height_deg               =  atan( (32.5 / 2) / 99.0) / pi*180*2;   % 18.64 deg 
         width                    = atan( (52.0 / 2) / 99.0) / pi*180*2';   % 29.43 deg 
@@ -87,9 +79,21 @@ switch env
         % eyelink
         el_monitor_size          = [-260.0, 162.5, 260.0, -162.5];         % monitor size in millimeters (center to left, top, right, and bottom). Numbers come from [32.5 cm height, 52 cm width] --> [325 cm height, 520 cm width] * 0.5
         el_screen_distance       = [1003 1003];                            % distance in millimeters from eye to top and bottom edge of the monitor. Given the 99 cm viewing distance, this is calculated as:  sqrt(99^2+(32.5/2)^2)*10 and then rounded to nearest integer.
-        point2point_distance_pix = ppdeg * 4;                              % 4 degrees
+        point2point_distance_pix = round(ppdeg * 4);                       % C/V targets are 4 degrees away from center
         cv_proportion_area       = 88;                                     % ???
 end
+
+
+stimuli_dir = [git_dir, '/stimuli'];
+run_filedir = [git_dir, sprintf('/data/sub%.02d/run%.02d', subjn, runn)];
+if ~exist(run_filedir, 'dir')
+    mkdir(run_filedir)
+end
+subj_filedir = [git_dir, sprintf('/data/sub%.02d/info', subjn)];
+if ~exist(subj_filedir, 'dir')
+    mkdir(subj_filedir)
+end
+
 
 % variables ---------------------------------------------------------------
 % unless debugging will use eyetracking / no skipsync
@@ -100,15 +104,16 @@ if ~exist('skipsynctest', 'var')
     skipsynctest = 0;
 end
 
+rng('shuffle');
+
 % get a timestamp
 datestring = datetime;
 datestring.Format = 'yyyyMMdd-HHmmss';
 datestring = string(datestring);
 
-% output file names here  
+% output file names
 eyefilename        = [run_filedir '/' sprintf('eye_sub%.02d_run%.02d_%s.edf', subjn, runn, datestring)];
 matfilename        = [run_filedir '/' sprintf('mat_sub%.02d_run%.02d_%s.mat', subjn, runn, datestring)];       
-% anything else? output dir, output file names? 
 
 %% set up run and images 
 %collect run file ---------------------------------------------------------
@@ -124,7 +129,7 @@ else
     end
     % load it in to continue
     load([subj_filedir '/' runfile.name]);
-    assert(exist('image_matrix', 'var'), 'problem loading run file'); 
+    assert(isequal(exist('image_matrix', 'var'), 1), 'problem loading run file'); % weird logical error in PP room fix 
 end
 
 % gather the info on this run ---------------------------------------------
@@ -152,8 +157,8 @@ fprintf('*** The next task will be %s ***\n', taskstring); % let the user prepar
 
 % run the experiment
 [timeframes,timekeys,digitrecord,trialoffsets] = ptviewmovie(images, image_order, [], frame_duration, [], [], zeros(5, 5), 128); 
-% do we save everything or just some variables?? 
-save(matfilename, 'run_info', 'timeframes', 'timekeys', 'digitrecord', 'trialoffsets');
+% save!!!!!!
+save(matfilename, 'run_info', 'timeframes', 'timekeys', 'digitrecord', 'trialoffsets', 'images', 'image_order', 'frame_duration');
 %saveexcept(filename,{'a1'});  % save immediately to prevent data loss
 fprintf('Experiment took %.5f seconds.\n',mean(diff(timeframes))*length(timeframes));
 
